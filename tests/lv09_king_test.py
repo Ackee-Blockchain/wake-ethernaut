c0934a062d7764c06e956ccde111c4b4d61d15f6
honzaprevratil <honzikprevratil@gmail.com>
2024-03-26 20:00:13 +0100

    🐛 Solution checking fixed

--- tests/lv09_king_test.py
+++ tests/lv09_king_test.py
@@ -7,11 +7,12 @@ from pytypes.contracts.lv09_king import King
 def test_lv09():
     ethernaut = EthernautDeployer(default_chain)
     contract = ethernaut.deploy_lv09()
-    exploit_lv09(contract)
-    ethernaut.check_lv09(contract)
+    newOwner = exploit_lv09(contract, ethernaut.attacker)
+    ethernaut.check_lv09(contract, newOwner)
 
-def exploit_lv09(contract: King):
+def exploit_lv09(contract: King, attacker: Account) -> Address:
     # TODO Became the new king and break the game - no one can claim the kingship from you.
+    # TODO Return the address of the new king.
     # TODO You can import your our own smart contract(s) here.
     # TODO Code here ...
-    pass
+    return attacker.address
