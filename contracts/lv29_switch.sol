a324bdf74f3a7a0932ad3985702498fb894dea12
Lukas Rajnoha <lukasino222@gmail.com>
2024-04-10 10:54:45 +0200

    ✏️ added comment for modified pragma setting in lv29

--- contracts/lv29_switch.sol
+++ contracts/lv29_switch.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.0;
+// solc set explicitly to 0.8.0 to avoid anvil issues
+// this does not affect the solution
 
 contract Switch {
     bool public switchOn; // switch is off
